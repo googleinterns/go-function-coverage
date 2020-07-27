a6ef5b68b399737ea8d5c7c24f9a72136cbe6cad
muratekici <muratekici99@gmail.com>
2020-07-27 13:13:21 +0300

    Initial implementation commit
    first working version of funccover

---
+++ pkg/instrument/func.go
@@ -0,0 +1,60 @@
+//Copyright 2020 Google LLC
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+package instrument
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+// FuncCoverBlock contains tha name and line of a function
+// Line contains the first line of the definition in the source code
+type FuncCoverBlock struct {
+	Name string // Name of the function
+	Line uint32 // Line number for block start.
+}
+
+// FuncCover contains the FuncCoverBlock informations of a source code
+type FuncCover struct {
+	FuncBlocks []FuncCoverBlock
+}
+
+// SaveFuncs parses given source code and returns a FuncCover instance
+func SaveFuncs(content []byte) FuncCover {
+
+	fset := token.NewFileSet()
+
+	parsedFile, err := parser.ParseFile(fset, "", content, parser.ParseComments)
+	if err != nil {
+		return FuncCover{}
+	}
+
+	var funcCover FuncCover
+
+	// Find function declerations to instrument and save them to funcCover
+	for _, decl := range parsedFile.Decls {
+		switch t := decl.(type) {
+		// Function Decleration
+		case *ast.FuncDecl:
+			funcCover.FuncBlocks = append(funcCover.FuncBlocks, FuncCoverBlock{
+				Name: t.Name.Name,
+				Line: uint32(fset.Position(t.Pos()).Line),
+			})
+		}
+	}
+
+	return funcCover
+}
