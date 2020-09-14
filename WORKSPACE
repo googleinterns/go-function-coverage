f2b903d5e2e28241e2ccaf8b78963d262a14a9c9
muratekici <muratekici99@gmail.com>
2020-09-14 13:45:16 +0300

    Initial commit for Bazel Integration TODO: README.md file will be edited

---
+++ WORKSPACE
@@ -0,0 +1,12 @@
+workspace(name = "go_monorepo")
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+git_repository(
+    name = "io_bazel_rules_go",
+    remote = "https://github.com/muratekici/rules_go.git",
+    commit = "11ec19e2507107881919b41038a811076b75907f",
+)
+
+load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
+go_rules_dependencies()
+go_register_toolchains()
---
+++ packages/program/functions/func.go
@@ -0,0 +1,25 @@
+//Copyright 2020 Google LLC
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+// Package covcollect implements a package to collect the function coverage data
+package functions
+
+import "fmt"
+
+func F0() { fmt.Println("F0 is executed") }
+func F1() { fmt.Println("F1 is executed") }
+func F2() { fmt.Println("F2 is executed") }
+func F3() { fmt.Println("F3 is executed") }
+func F4() { fmt.Println("F4 is executed") }
+func F5() { fmt.Println("F5 is executed") }
